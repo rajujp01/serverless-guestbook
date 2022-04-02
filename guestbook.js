1e576cd6325d646043b51dd88daa1fb9d2e57eb3
rajujp01 <42689057+rajujp01@users.noreply.github.com>
2022-04-02 16:23:06 +0800

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://aa31a92c.au-syd.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
